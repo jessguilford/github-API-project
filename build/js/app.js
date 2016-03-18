3680ddd14a46b93245c1c8acb55fdaa145792ab0
Jessie Guilford <Jessie Guilford>
2016-03-18 13:20:09 -0700

    create nexted for loop to log repo names

--- build/js/app.js
+++ build/js/app.js
@@ -5,19 +5,25 @@ exports.apiKey = "05666ec5e7c6afa1db533ecb8e369a718bc00b53";
 var apiKey = require("./../.env").apiKey;
 
 exports.getRepos = function(input) {
-  var userArray = [];
-  $.get('https://api.github.com/users/' + input + '?access_token=' + apiKey).then(function(response) {
-    userArray.push(response);
+  var repoArray = [];
+  $.get('https://api.github.com/users/' + input + "/repos" + '?access_token=' + apiKey).then(function(response) {
+    repoArray.push(response);
 
-    for(var i = 0; i < userArray.length; i++) {
-      console.log(userArray[i]);
-      var output = document.getElementById('output');
-      output.innerHTML = "<li>" + userArray[i].name + "</li>";
+    for(var i = 0; i < repoArray.length; i++) {
+      console.log(repoArray[i]);
+      for(var j = 0; j < repoArray[i].length; j++) {
+        console.log(repoArray[i][j].name);
+      }
     }
+
   }).fail(function(error) {
     console.log(error.responseJSON.message);
   });
 };
+
+
+// var output = document.getElementById('output');
+// output.innerHTML = output.innerHTML + "<li>" + userArray[i][j].name + "</li>";
 
 // exports.printRepos = function(userArray) {
 //   for(var i=1; i < userArray.length; i++) {
